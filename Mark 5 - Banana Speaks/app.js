a067d653101d6b0cc941020f942613e4e78633ce
Pratham <ppratham812@gmail.com>
2023-03-08 14:12:05 +0530

    Mark5 project started

---
+++ Mark 5 - Banana Speaks/app.js
@@ -0,0 +1,9 @@
+// let username = prompt("What is your name?");
+// let welomemsg = `This script works ${username}`;
+// alert(welomemsg);
+
+let btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate);
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log(`clicked`);
+})
