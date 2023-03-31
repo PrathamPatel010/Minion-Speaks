466276546611f61a95c3ce2daf5d8e158afbd3dc
Pratham <ppratham812@gmail.com>
2023-03-31 19:53:06 +0530

    Files renamed

--- MARK0 Guess the number/index.js
+++ MARK0 Guess the number/index.js
@@ -1,29 +1,29 @@
 // required setup to use prompt in vscode
-const prompt = require("prompt-sync")({sigint:true})
+const prompt = require("prompt-sync")({ sigint: true })
 
 
 // function to generate random number within range
 function getNum(max = 100) {
     let random = Math.floor(Math.random() * max)
     return random
-  }
+}
 let num = getNum()
-  
+
 let correct = false
 let count = Number.parseInt(0)
 while (!correct) {
     let guess = prompt("Guess the number: ")
     if (guess > num) {
-      count++
-      console.log("Please enter lesser number")
+        count++
+        console.log("Please enter lesser number")
     } else if (guess < num) {
-      count++
-      console.log("Please enter greater number")
+        count++
+        console.log("Please enter greater number")
     } else {
-      count++
-      correct = true
-      console.log("You guessed the correct number")
-      let score = Number.parseInt(100 - count)
-      console.log("Your score is " + score)
+        count++
+        correct = true
+        console.log("You guessed the correct number")
+        let score = Number.parseInt(100 - count)
+        console.log("Your score is " + score)
     }
 }
